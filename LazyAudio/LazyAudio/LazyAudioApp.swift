672b217d520c6c54417df0913654ad8be175209a
momaek <momaek17@gmail.com>
2025-06-07 15:02:39 +0800

    Add Design doc and init swift

---
+++ LazyAudio/LazyAudio/LazyAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyAudioApp.swift
+//  LazyAudio
+//
+//  Created by wentx on 2025/6/7.
+//
+
+import SwiftUI
+
+@main
+struct LazyAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
